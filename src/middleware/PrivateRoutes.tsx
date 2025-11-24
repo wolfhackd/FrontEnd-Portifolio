68fbd7dcd3de1d82835a1c73e0454edf6c4af462
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-11-24 00:31:07 -0300

    fix: me

--- src/middleware/PrivateRoutes.tsx
+++ src/middleware/PrivateRoutes.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { useEffect, useState, type ReactElement } from 'react';
 import { Navigate } from 'react-router-dom';
 
@@ -5,23 +6,41 @@ export function PrivateRoute({ children }: { children: ReactElement }) {
   const [loading, setLoading] = useState(true);
   const [authenticated, setAuthenticated] = useState(false);
 
+  // useEffect(() => {
+  //   fetch(`${import.meta.env.VITE_API}/me`, {
+  //     method: 'GET',
+  //     credentials: 'include',
+  //   })
+  //     .then(async (res) => {
+  //       if (res.ok) {
+  //         setAuthenticated(true);
+  //       } else {
+  //         setAuthenticated(false);
+  //       }
+  //       setLoading(false);
+  //     })
+  //     .catch(() => {
+  //       setAuthenticated(false);
+  //       setLoading(false);
+  //     });
+  // }, []);
+
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_API}/me`, {
-      method: 'GET',
-      credentials: 'include',
-    })
-      .then(async (res) => {
-        if (res.ok) {
-          setAuthenticated(true);
-        } else {
-          setAuthenticated(false);
-        }
-        setLoading(false);
-      })
-      .catch(() => {
+    async function checkAuth() {
+      try {
+        const res = await axios.get(`${import.meta.env.VITE_API}/me`, {
+          withCredentials: true,
+        });
+
+        setAuthenticated(res.status === 200);
+      } catch {
         setAuthenticated(false);
+      } finally {
         setLoading(false);
-      });
+      }
+    }
+
+    checkAuth();
   }, []);
 
   if (loading) return <div>Carregando...</div>;
